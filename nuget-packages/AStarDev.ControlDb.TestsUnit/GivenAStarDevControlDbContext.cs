a87d200118e5b8c9305dbf3ed87c92a5d29be02f
Jason Barden <jason.barden@outlook.com>
2026-09-22 16:28:16 +0100

    [RED] failing test for creating the ControlDbContext
    fails as the class doesn't exist yet lol

---
+++ nuget-packages/AStarDev.ControlDb.TestsUnit/GivenAStarDevControlDbContext.cs
@@ -0,0 +1,8 @@
+namespace AStarDev.ControlDb.TestsUnit;
+
+public class GivenAStarDevControlDbContext
+{
+    [Fact]
+    public void when_creating_a_new_instance_it_is_created_without_error()
+        => new ControlDbContext().ShouldNotBeNull();
+}
