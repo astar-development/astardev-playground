ce4b547eedf17992a472d6026b14c0bcba7f03d1
Jason Barden <jason.barden@outlook.com>
2026-09-22 16:33:30 +0100

    [RED] failing test for controlDbContext being DbContext

--- nuget-packages/AStarDev.ControlDb.TestsUnit/GivenAStarDevControlDbContext.cs
+++ nuget-packages/AStarDev.ControlDb.TestsUnit/GivenAStarDevControlDbContext.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace AStarDev.ControlDb.TestsUnit;
 
 public class GivenAStarDevControlDbContext
@@ -5,4 +7,8 @@ public class GivenAStarDevControlDbContext
     [Fact]
     public void when_creating_a_new_instance_it_is_created_without_error()
         => new ControlDbContext().ShouldNotBeNull();
+
+    [Fact]
+    public void when_creating_a_new_instance_it_is_created_by_extending_the_dbContext()
+        => new ControlDbContext().ShouldBeAssignableTo<DbContext>();
 }
