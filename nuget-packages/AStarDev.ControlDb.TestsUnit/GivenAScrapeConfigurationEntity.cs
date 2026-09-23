bf45ed691c13cdf6d7147b02afe0f0266300c2c9
Jason Barden <jason.barden@outlook.com>
2026-09-23 14:36:09 +0100

    feat: update CI workflow to simplify test command and remove obsolete unit test

---
+++ nuget-packages/AStarDev.ControlDb.TestsUnit/GivenAScrapeConfigurationEntity.cs
@@ -0,0 +1,21 @@
+using AStarDev.ControlDb.TestsUnit.TestResults;
+
+namespace AStarDev.ControlDb.TestsUnit;
+
+public sealed class GivenAScrapeConfigurationDomainModel
+{
+    [Fact]
+    public void when_creating_a_domain_model_then_all_properties_are_set_correctly()
+    {
+        ScrapeConfiguration entity = ScrapeConfigurationFactory.Create();
+
+        entity.SiteName.ShouldBe("Mock Site Name");
+        entity.SiteUrl.ShouldBe("https://example.com");
+        entity.SearchCategoryPrefix.ShouldBe("Mock Search Category Prefix");
+        entity.SearchCategorySuffix.ShouldBe("Mock Search Category Suffix");
+        entity.TopWallpapers.ShouldBe("Mock Top Wallpapers");
+        entity.HotWallpapers.ShouldBe("Mock Hot Wallpapers");
+        entity.Username.ShouldBe("Mock Username");
+        entity.HashedPassword.ShouldBe("Mock Hashed Password");
+    }
+}
