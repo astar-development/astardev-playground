46c8148b9f2ed3fb6f476f326523096f04c70373
Jason Barden <jason.barden@outlook.com>
2026-09-23 12:30:48 +0100

    fix previous commit tests and vscode / code-coverage.sh so they no-longer error on generated code

--- nuget-packages/AStarDev.ControlDb.Persistence.TestsIntegration/GivenAnInstanceOfScrapeConfigurations.cs
+++ nuget-packages/AStarDev.ControlDb.Persistence.TestsIntegration/GivenAnInstanceOfScrapeConfigurations.cs
@@ -1,27 +1,46 @@
 using AStarDev.ControlDb.Persistence.TestsIntegration.TestFactories;
 using AStarDev.Utilities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace AStarDev.ControlDb.Persistence.TestsIntegration;
 
-public class GivenAnInstanceOfScrapeConfigurations
+public sealed class GivenAnInstanceOfScrapeConfigurations : IDisposable
 {
+    private readonly SqliteConnection connection;
+    private readonly DbContextOptions<ControlDbContext> options;
+
     public GivenAnInstanceOfScrapeConfigurations()
     {
-        var context = new ControlDbContext();
-        context.Database.EnsureDeleted();
+        connection = new SqliteConnection("Data Source=:memory:");
+        connection.Open();
+        options = new DbContextOptionsBuilder<ControlDbContext>().UseSqlite(connection).Options;
+
+        using var context = new ControlDbContext(options);
         context.Database.EnsureCreated();
     }
 
     [Fact]
     public void when_accessing_the_scrapeConfigurations_dbSet_it_should_not_be_null()
     {
-        var context = new ControlDbContext();
+        using var context = new ControlDbContext(options);
         ScrapeConfiguration entity = ScrapeConfigurationFactory.Create();
         context.ScrapeConfigurations.Add(entity);
         context.SaveChanges();
 
-        var newContext = new ControlDbContext();
+        using var newContext = new ControlDbContext(options);
+        var result = newContext.ScrapeConfigurations.First();
 
-        newContext.ScrapeConfigurations.First().ToJson().ShouldMatchApproved();
+        result.Id.ShouldBeAssignableTo<ScrapeConfigurationId>();
+        result.HashedPassword.ShouldBe("Mock Hashed Password");
+        result.HotWallpapers.ShouldBe("Mock Hot Wallpapers");
+        result.Username.ShouldBe("Mock Username");
+        result.TopWallpapers.ShouldBe("Mock Top Wallpapers");
+        result.SearchCategoryPrefix.ShouldBe("Mock Search Category Prefix");
+        result.SearchCategorySuffix.ShouldBe("Mock Search Category Suffix");
+        result.SiteName.ShouldBe("Mock Site Name");
+        result.SiteUrl.ShouldBe("https://example.com");
     }
+
+    public void Dispose() => connection.Dispose();
 }
--- nuget-packages/AStarDev.ControlDb.Persistence.TestsIntegration/TestFactories/ScrapeConfigurationFactory.cs
+++ nuget-packages/AStarDev.ControlDb.Persistence.TestsIntegration/TestFactories/ScrapeConfigurationFactory.cs
@@ -3,5 +3,5 @@ namespace AStarDev.ControlDb.Persistence.TestsIntegration.TestFactories;
 public static class ScrapeConfigurationFactory
 {
     public static ScrapeConfiguration Create()
-        => new(ScrapeConfigurationId.Empty, "Mock Site Name", "https://example.com", "Mock Search Category Prefix", "Mock Search Category Suffix", "Mock Top Wallpapers", "Mock Hot Wallpapers");
+        => new(ScrapeConfigurationId.Empty, "Mock Site Name", "https://example.com", "Mock Search Category Prefix", "Mock Search Category Suffix", "Mock Top Wallpapers", "Mock Hot Wallpapers", "Mock Username", "Mock Hashed Password");
 }
